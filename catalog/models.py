7b42818e4384eda510d2925b94e132b2ace15b57
Tatiana Chepurnaya <tanyusha_tokor@mail.ru>
2024-05-01 22:20:01 +0500

    add models and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', null=True, blank=True)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.FloatField(verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+    manufactured_at = models.DateTimeField(default='1000-01-01', verbose_name='дата производства продукта')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
