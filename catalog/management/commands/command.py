b1c2d685f2397e660bddd270fe9451ad4ae49f25
Tatiana Chepurnaya <tanyusha_tokor@mail.ru>
2024-05-05 18:50:22 +0500

    add code homework 20.1

---
+++ catalog/management/commands/command.py
@@ -0,0 +1,48 @@
+import json
+
+from django.core.management import BaseCommand
+
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories():
+        with open('fixture/category_data.json') as f:
+            json_data = json.load(f)
+            categories = []
+            for data in json_data:
+                categories.append(data['fields'])
+            return categories
+
+    @staticmethod
+    def json_read_products():
+        with open('fixture/product_data.json') as f:
+            json_data = json.load(f)
+            products = []
+            for data in json_data:
+                products.append(data['fields'])
+            return products
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        product_for_create = []
+        category_for_create = []
+
+        for category in Command.json_read_categories():
+            category_for_create.append(Category(**category))
+
+        Category.objects.bulk_create(category_for_create)
+
+        for product in Command.json_read_products():
+            product_for_create.append(
+                Product(name=product['name'],
+                        description=product['description'],
+                        category=Category.objects.get(name='яблоки'),
+                        price=product['price'])
+            )
+
+        Product.objects.bulk_create(product_for_create)
--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
-    description = models.TextField(verbose_name='описание', null=True, blank=True)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -15,13 +18,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
-    description = models.TextField(verbose_name='описание', null=True, blank=True)
-    image = models.ImageField(upload_to='products/', verbose_name='изображение', null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, verbose_name='категория', **NULLABLE)
     price = models.FloatField(verbose_name='цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
-    manufactured_at = models.DateTimeField(default='1000-01-01', verbose_name='дата производства продукта')
+    # manufactured_at = models.DateTimeField(default='1000-01-01', verbose_name='дата производства продукта')
 
     def __str__(self):
         return f'{self.name}'
